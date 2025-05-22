cbfc74ab8f78182f666030874f04225691d72128
Rubina Rai <161278262+nezuruby@users.noreply.github.com>
2025-05-22 15:37:03 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <h1>Hello from Rubina!</h1>
+<p>This change proves that auto-deploy works in vercel!</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
